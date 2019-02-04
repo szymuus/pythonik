ceccf5cf7f68f8ade2d14e1250767912d9c4e033
szymuus <szymoooneek@gmail.com>
2019-02-04 20:44:56 +0100

    new url

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("heeelo")
-# Create your views here.
+def post_list(request):
+	return render(request,'blog/post_list.html',{})
+
+
